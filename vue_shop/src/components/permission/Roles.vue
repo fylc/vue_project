31bcf0df12ac5624bc2839a9df85167d04d27281
方圆 <fylc21@126.com>
2022-04-12 05:49:39 +0800

    分配权限功能：权限列表渲染完成

--- vue_shop/src/components/permission/Roles.vue
+++ vue_shop/src/components/permission/Roles.vue
@@ -122,12 +122,39 @@
 							type="warning"
 							icon="el-icon-setting"
 							size="mini"
+							@click="showSetRightDialog(slotProps.row)"
 							>分配权限</el-button
 						>
 					</template>
 				</el-table-column>
 			</el-table>
 		</el-card>
+
+		<!-- 分配权限的对话框 -->
+		<el-dialog
+			title="分配权限"
+			:visible.sync="setRightDialogVisible"
+			width="50%"
+      @close="setRightDialogClosed"
+		>
+			<!-- 树形空间 -->
+			<el-tree
+				show-checkbox
+				:data="rightslist"
+				:props="treeProps"
+				node-key="id"
+				:default-expand-all="true"
+				:default-checked-keys="defKeys"
+			></el-tree>
+			<span slot="footer" class="dialog-footer">
+				<el-button @click="setRightDialogVisible = false"
+					>取 消</el-button
+				>
+				<el-button type="primary" @click="setRightDialogVisible = false"
+					>确 定</el-button
+				>
+			</span>
+		</el-dialog>
 	</div>
 </template>
 
@@ -137,6 +164,19 @@ export default {
 		return {
 			// 所有角色的列表数据
 			roleList: [],
+			// 控制分配权限对话框的显示与隐藏
+			setRightDialogVisible: false,
+			// 所有权限的数据
+			rightslist: [],
+			// 树形控件的属性绑定对象
+			treeProps: {
+				// 表示在树形控件中展示的属性值
+				label: 'authName',
+				// 子级依赖的属性
+				children: 'children',
+			},
+			// 默认选中的节点id值数组
+			defKeys: [],
 		};
 	},
 	created() {
@@ -183,6 +223,30 @@ export default {
 					});
 				});
 		},
+
+		// 展示分配权限的对话框
+		async showSetRightDialog(role) {
+			// 获取所有权限的数据
+			const { data: result } = await this.$http.get(`rights/tree`);
+			if (result.meta.status !== 200)
+				return this.$message.error('获取所有权限数据失败');
+			// 获取权限数据成功后将数据赋值给rightlist
+			this.rightslist = result.data;
+      // 递归获取三级节点的id
+      this.getLeafKeys(role, this.defKeys)
+			this.setRightDialogVisible = true;
+		},
+		// 通过递归的形式，获取角色下所有三级权限的id，并保存到defKeys数组中 node 要操作的节点 arr 存储id的数组
+		getLeafKeys(node, arr) {
+      // 如果当前node节点不包含children 属性，则是三级节点
+			if (!node.children) return arr.push(node.id);
+      node.children.forEach(item=> this.getLeafKeys(item, arr))
+		},
+    // 监听分配权限对话框的关闭事件
+    setRightDialogClosed() {
+      // 每一次关闭分配权限的对话框，都要把当前这一轮中绑定的默认选中的节点id值数组清空
+      this.defKeys = []
+    }
 	},
 };
 </script>
