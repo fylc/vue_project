f59cce07ab0940816eb97c6c1d8f66bbb50cec79
方圆 <fylc21@126.com>
2022-04-24 14:57:20 +0800

    hrsass

---
+++ hrsass/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <router-view />
+    <!-- <input v-model="userinfo" type="text" title="aaaa" style="width: 300px; height: 300px; background: red"> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
